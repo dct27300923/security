fabfbe1d5693c01e8d73562eb34232f62c4bf710
dct27300923 <53962670+dct27300923@users.noreply.github.com>
2021-04-17 23:14:22 +0900

    Update main.cpp

--- MOD/main.cpp
+++ MOD/main.cpp
@@ -3,27 +3,26 @@
 #include <fstream>
 #include <time.h>
 #include <chrono>
-int gdb(int a, int b)
-{
-	int temp, x = a, y = b;
-	while (y != 0)
-	{
-		temp = y;
-		y = x % y;
-		x = temp;
-	}
-	return x;
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	return gcd(b, a % b);
 }
 
 int randPrime(int n)
 {
 
 	int a = rand();
-	while (gdb(a, n) != 1)
+	while (gcd(a, n) != 1)
 	{
 		a = rand();
 	}
 	return a;
+}
+long long pow(long long k, int x, int N) {
+	if (x == 0) return 1;
+	if (x == 1) return k;
+	if (x % 2 == 1) return (k * pow(k*k % N, x / 2, N)) % N;
+	if (x % 2 == 0) return pow(k*k %N, x / 2, N) % N;
 }
 int main(void)
 {
@@ -36,7 +35,6 @@ int main(void)
 	int rangeMin = 10000;
 	std::ofstream output("./test_6.txt");
 	clock_t start, end;
-
 	for (int i = 0; i < 100; i++)
 	{
 		N = (int)(((double)((rand() << 15) | rand())) / (((RAND_MAX << 15) | RAND_MAX) + 1) * (rangeMax - rangeMin) + rangeMin);
@@ -45,13 +43,13 @@ int main(void)
 
 		int x = 1;
 		std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-		for (int result = k % N; result != 1; ++x)
+		for (long long result = k % N; result != 1; ++x)
 		{
-			result = (result * k) % N;
+			result = pow(result, x, N);
 		}
 		std::chrono::duration<double> sec = std::chrono::system_clock::now() - start;
-		std::cout << "<" << i << ">" << k << "^" << x << " mod " << N << " = 1 | time : " << sec.count() << "ÃÊ\n";
-		output << "<" << i << ">" << k << "^" << x << " mod " << N << " = 1 | time : " << sec.count() << "ÃÊ\n";
+		std::cout << "<" << i << ">" << k << "^" << x << " mod " << N << " = 1 | time : " << sec.count() << "ï¿½ï¿½\n";
+		output << "<" << i << ">" << k << "^" << x << " mod " << N << " = 1 | time : " << sec.count() << "ï¿½ï¿½\n";
 	}
 
 	return 0;
